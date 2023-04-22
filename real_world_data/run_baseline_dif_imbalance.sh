7a7496f7b21a705c7b8421dc6908e1b927f09643
ChrisXue <41327917+chrisyxue@users.noreply.github.com>
2023-04-22 14:06:44 -0400

    Update run_baseline_dif_imbalance.sh

--- real_world_data/run_baseline_dif_imbalance.sh
+++ real_world_data/run_baseline_dif_imbalance.sh
@@ -2,18 +2,12 @@
 
 # cd /mnt/home/xuezhiyu/AdaFair_Final
 
-# /mnt/home/xuezhiyu/anaconda3/bin/python main_baselines_dif_imbalance.py --dataset Adult --n_estimators 200 
-# /mnt/home/xuezhiyu/anaconda3/bin/python main_baselines_dif_imbalance.py --dataset bank --n_estimators 200
-# /mnt/home/xuezhiyu/anaconda3/bin/python main_baselines_dif_imbalance.py --dataset kdd --n_estimators 200
-# /mnt/home/xuezhiyu/anaconda3/bin/python main_baselines_dif_imbalance.py --dataset compas --n_estimators 200
-# /mnt/home/xuezhiyu/anaconda3/bin/python main_baselines_dif_imbalance.py --dataset credit --n_estimators 200
 
-
-python main_baselines_dif_imbalance.py --dataset Adult --n_estimators 200 
-python main_baselines_dif_imbalance.py --dataset bank --n_estimators 200
-python main_baselines_dif_imbalance.py --dataset kdd --n_estimators 200
-python main_baselines_dif_imbalance.py --dataset compas --n_estimators 200
-python main_baselines_dif_imbalance.py --dataset credit --n_estimators 200
+python main_baselines_dif_imbalance.py --dataset Adult --n_estimators 200 --save_path <your-file-path>
+python main_baselines_dif_imbalance.py --dataset bank --n_estimators 200 --save_path <your-file-path>
+python main_baselines_dif_imbalance.py --dataset kdd --n_estimators 200 --save_path <your-file-path>
+python main_baselines_dif_imbalance.py --dataset compas --n_estimators 200 --save_path <your-file-path>
+python main_baselines_dif_imbalance.py --dataset credit --n_estimators 200 --save_path <your-file-path>
 
 
 
