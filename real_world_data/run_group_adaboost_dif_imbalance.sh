3a6c4f21ac593cc3f50269644b5fbb0aeb650a34
ChrisXue <41327917+chrisyxue@users.noreply.github.com>
2023-04-22 14:07:36 -0400

    Update run_group_adaboost_dif_imbalance.sh

--- real_world_data/run_group_adaboost_dif_imbalance.sh
+++ real_world_data/run_group_adaboost_dif_imbalance.sh
@@ -1,18 +1,18 @@
 #!/bin/bash 
 
-python main_group_adaboost_dif_imbalance.py --dataset compas --n_estimators 100 --depth 2
-python main_group_adaboost_dif_imbalance.py --dataset bank --n_estimators 100 --depth 2
-python main_group_adaboost_dif_imbalance.py --dataset credit --n_estimators 100 --depth 2
-python main_group_adaboost_dif_imbalance.py --dataset Adult_NM --n_estimators 100 --depth 2
-python main_group_adaboost_dif_imbalance.py --dataset Adult_MI --n_estimators 100 --depth 2
-python main_group_adaboost_dif_imbalance.py --dataset Adult_CA --n_estimators 100 --depth 2
-python main_group_adaboost_dif_imbalance.py --dataset Adult_PA --n_estimators 100 --depth 2
+python main_group_adaboost_dif_imbalance.py --dataset compas --n_estimators 100 --depth 2 --save_path <your-file-path>
+python main_group_adaboost_dif_imbalance.py --dataset bank --n_estimators 100 --depth 2 --save_path <your-file-path>
+python main_group_adaboost_dif_imbalance.py --dataset credit --n_estimators 100 --depth 2 --save_path <your-file-path>
+python main_group_adaboost_dif_imbalance.py --dataset Adult_NM --n_estimators 100 --depth 2 --save_path <your-file-path>
+python main_group_adaboost_dif_imbalance.py --dataset Adult_MI --n_estimators 100 --depth 2 --save_path <your-file-path>
+python main_group_adaboost_dif_imbalance.py --dataset Adult_CA --n_estimators 100 --depth 2 --save_path <your-file-path>
+python main_group_adaboost_dif_imbalance.py --dataset Adult_PA --n_estimators 100 --depth 2 --save_path <your-file-path>
 
-python main_group_adaboost_dif_imbalance.py --dataset compas --n_estimators 200 --depth 2
-python main_group_adaboost_dif_imbalance.py --dataset bank --n_estimators 200 --depth 2
-python main_group_adaboost_dif_imbalance.py --dataset credit --n_estimators 200 --depth 2
-python main_group_adaboost_dif_imbalance.py --dataset Adult_NM --n_estimators 200 --depth 2
-python main_group_adaboost_dif_imbalance.py --dataset Adult_MI --n_estimators 200 --depth 2
-python main_group_adaboost_dif_imbalance.py --dataset Adult_CA --n_estimators 200 --depth 2
-python main_group_adaboost_dif_imbalance.py --dataset Adult_PA --n_estimators 200 --depth 2
+python main_group_adaboost_dif_imbalance.py --dataset compas --n_estimators 200 --depth 2 --save_path <your-file-path>
+python main_group_adaboost_dif_imbalance.py --dataset bank --n_estimators 200 --depth 2 --save_path <your-file-path>
+python main_group_adaboost_dif_imbalance.py --dataset credit --n_estimators 200 --depth 2 --save_path <your-file-path>
+python main_group_adaboost_dif_imbalance.py --dataset Adult_NM --n_estimators 200 --depth 2 --save_path <your-file-path>
+python main_group_adaboost_dif_imbalance.py --dataset Adult_MI --n_estimators 200 --depth 2 --save_path <your-file-path>
+python main_group_adaboost_dif_imbalance.py --dataset Adult_CA --n_estimators 200 --depth 2 --save_path <your-file-path>
+python main_group_adaboost_dif_imbalance.py --dataset Adult_PA --n_estimators 200 --depth 2 --save_path <your-file-path>
 
